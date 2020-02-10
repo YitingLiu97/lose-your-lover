db126bf73d32fdc382261b4754a4d0035b1a6b4b
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2020-02-10 03:26:27 -0500

    randomize color for the border under onclick

--- sketch.js
+++ sketch.js
@@ -1,6 +1,24 @@
 //get mouseposition, display different colors of the red squares
 //click to change the background color 
 
-var colorBar;
-var colorBg;
+function showCoords(event) {
+    var cX = event.clientX;
+    var cY = event.clientY;
+    document.getElementById("demo").style.backgroundColor = 'green';
+    document.getElementById("demo2").style.backgroundColor = 'purple';
 
+  }
+
+  function randomColor(){
+   
+      var randColorValue ='#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+      var randColorValue2 ='#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+      document.getElementById("demo").style.backgroundColor=randColorValue;
+      document.getElementById("demo2").style.backgroundColor=randColorValue2;
+
+      // var a = ;
+      // var b = 0;
+      // var c = 30;
+      // document.getElementById("demo").style.backgroundColor="rgb(" + a + "," + b + "," + c + ")";
+
+  }
